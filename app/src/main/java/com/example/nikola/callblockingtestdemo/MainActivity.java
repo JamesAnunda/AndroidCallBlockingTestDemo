b67109d64c0dc682ab24b976f709a8c3d093cd67
Chris Anunda <cjanunda@gmail.com>
2018-09-17 17:12:11 -0400

    Backendless
    Configured Backendless for the App

--- app/src/main/java/com/example/nikola/callblockingtestdemo/MainActivity.java
+++ app/src/main/java/com/example/nikola/callblockingtestdemo/MainActivity.java
@@ -6,8 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import com.backendless.Backendless;
+
 public class MainActivity extends AppCompatActivity {
     private static final int PERMISSION_REQUEST_READ_PHONE_STATE = 1;
+    public static final String APP_ID = "D5BC0FC3-3F12-964B-FF77-378A18BA7300";
+    public static final String SECERT_KEY = "B1A3B987-86B9-B030-FFA4-EDE7BB7E0700";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,7 +26,9 @@ public class MainActivity extends AppCompatActivity {
                 requestPermissions(permissions, PERMISSION_REQUEST_READ_PHONE_STATE);
             }
         }
+        Backendless.initApp(APP_ID,SECERT_KEY);
     }
+
 
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
